2a18fdd8b88e73a458070fe03363a9980036dfeb
Alex Porcena <79947822+alexporcena@users.noreply.github.com>
2022-01-09 12:57:31 -0300

    Add files via upload

---
+++ scripts/Plataforma.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const VELOCIDADE_PLATAFORMA = -100
+
+func _ready():
+	criar_plataforma(5)
+
+func criar_plataforma(celulas):
+	$TileMap.set_cell(0, 0, 0)
+
+	for celula in celulas:
+		$TileMap.set_cell(celula + 1, 0, 1)
+
+	$TileMap.set_cell(celulas + 1, 0, 2)
+	
+func _physics_process(delta):
+	position.x += VELOCIDADE_PLATAFORMA * delta
